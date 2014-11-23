ebdb702ca483b1982b52002be680db8e48ac092b
Fábio <fabiorochadacosta@gmail.com>
2014-11-23 15:45:07 -0200

    Refactor e correcoes

--- src/BagOfWords.java
+++ src/BagOfWords.java
@@ -1,119 +1,144 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author monica
- */
 public class BagOfWords {
+    private static Properties prop;
+    private static InputStream input = null;
 
-    /**
-     * @param args the command line arguments
-     */
+    private static ArrayList<String> bagOfWords;
+    private static File dirFiles;
+    private static List<File> files = new ArrayList<>();
+    private static List<File> textFolders = new ArrayList<>();
+    private static List<ArrayList<String>> listao = new ArrayList<>();
+    private static List<String> subjects = new ArrayList<>();
+    private static ArrayList<String> linhasTreino = new ArrayList<>();
+    private static ArrayList<String> linhasTeste = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
-        
-        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
-        String pathBag, pathDir,assunto;
-        File dirFiles, bag;
+        prop = new Properties();
+        input = new FileInputStream(System.getProperty("user.dir") + "\\src\\config.properties");
+        prop.load(input);
 
-        System.out.println("Caminho da BagOfWords:");
-        //pathBag = buf.readLine();
-        pathBag = "C:\\Users\\monica\\Desktop\\IA\\categorias_balanceadas\\bag_of_words_geral.txt";
-        bag = new File(pathBag);
+        System.out.println("Lendo BagOfWords:");
+        readBagOfWords();
 
-        System.out.println("Caminho dos Arquivos:");
-        pathDir = buf.readLine();
-        dirFiles = new File(pathDir);
-        
-        System.out.println("Nome assunto:");
-        assunto = buf.readLine();
+        System.out.println("Lendo Arquivos:");
+        readTextFiles();
 
-        List<File> files = new ArrayList<>();
+        for (final File fileEntry : textFolders) {
+            if (fileEntry.isDirectory()) {
+                listao = new ArrayList<>();
+                for (File file : fileEntry.listFiles()) {
+                    if (file.isFile()) {
+                        listOriginalWord(file);
+                    }
+                }
 
-        //Cria List com arquivos da pasta designada
-        for (File file : dirFiles.listFiles()) {
-            files.add(file);
+                String assunto = getSubject(fileEntry);
+                subjects.add(assunto);
+                writeBinaryLine(assunto);
+            }
         }
-        
-        //Cria list com as stopWords do arquivo
-        ArrayList<String> bagList = new ArrayList<>();
-        try {
-            FileReader fileReader = new FileReader(bag);
+        writeARFF(linhasTreino, "treino");
+        writeARFF(linhasTeste, "teste");
+    }
 
-            BufferedReader bufferedReader
-                    = new BufferedReader(fileReader);
+    private static void readBagOfWords() {
+        //Cria list com as stopWords do arquivo
+        bagOfWords = new ArrayList<>();
+        try {
+            FileReader fileReader = new FileReader(new File(prop.getProperty("pathBagOfWords")));
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
             String linha = "";
             while ((linha = bufferedReader.readLine()) != null) {
-                bagList.add(linha);
+                bagOfWords.add(linha);
             }
             fileReader.close();
             bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        List<ArrayList<String>> listao = new ArrayList<>();
+    }
 
-        for (File file : files) {
-            ArrayList<String> arqFiles = new ArrayList<>();
-            try {
-                FileReader fileReader = new FileReader(file);
+    private static void readTextFiles() {
+        dirFiles = new File(prop.getProperty("pathSubjects"));
+        for (final File fileEntry : dirFiles.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                textFolders.add(fileEntry);
+            }
+        }
+    }
 
-                BufferedReader bufferedReader
-                        = new BufferedReader(fileReader);
+    private static void listOriginalWord(File file) {
+        ArrayList<String> arqFiles = new ArrayList<>();
+        try {
+            BufferedReader bufferedReader;
+            try (FileReader fileReader = new FileReader(file)) {
+                bufferedReader = new BufferedReader(fileReader);
                 String linha = "";
                 while ((linha = bufferedReader.readLine()) != null) {
-                    linha = linha.replaceAll("[^A-Za-z]", "");
+                    linha = linha.replaceAll("[^A-Za-z ]", "");
 
                     arqFiles.add(linha);
                 }
-                fileReader.close();
-                bufferedReader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-            listao.add(arqFiles);
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        
-        ArrayList<String> binario = new ArrayList<String>();
-        
-        for(ArrayList<String> f: listao){
+        listao.add(arqFiles);
+    }
+
+    private static void writeBinaryLine(String assunto) {
+        int sizeTreino = (int) Math.round(listao.size() - (listao.size() * 0.2));
+        int cont = 1;
+
+        for (ArrayList<String> wordsInText : listao) {
             String bin = "";
-            for(String p: f){
-                
-                if(bagList.contains(p)){
-                    bin+=1+",";
-                }else{
-                    bin+=0+",";
+
+            int limit = getBagOfWordsLimit();
+            for (String wordInBag : bagOfWords.subList(0, limit)) {
+                if (wordsInText.contains(wordInBag)) {
+                    bin += 1 + ",";
+                } else {
+                    bin += 0 + ",";
                 }
             }
-            bin+=assunto;
-            binario.add(bin);            
+
+            bin += assunto;
+            if (cont <= sizeTreino) {
+                linhasTreino.add(bin);
+            } else {
+                linhasTeste.add(bin);
+            }
+
+            cont++;
         }
-        
-        //Cria arquivo para alimentar o arff
-        //cria arquivo de teste
+    }
+
+    private static void writeARFF(ArrayList<String> binario, String type) {
         try {
-            FileWriter fw = new FileWriter(pathDir + "linhas_"+assunto+".txt", true);
+            FileWriter fw = new FileWriter(dirFiles + "\\" + type + ".arff", false);
+
             BufferedWriter bw = new BufferedWriter(fw);
+            setFileHeaders(bw, type);
 
             for (String s : binario) {
-
-                    bw.write(s);
-                    bw.newLine();
+                bw.write(s);
+                bw.newLine();
             }
             bw.close();
             fw.close();
@@ -121,7 +146,51 @@ public class BagOfWords {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
     }
-    
+
+    private static String getSubject(File fileEntry) {
+        String[] pathSplit = fileEntry.getPath().split("\\\\"); //buf.readLine();
+        return pathSplit[pathSplit.length - 1];
+    }
+
+    private static void setFileHeaders(BufferedWriter bw, String type) {
+        try {
+            bw.write("@relation <tfIA2" + type + ">");
+            bw.newLine();
+            bw.newLine();
+            
+            int limit = getBagOfWordsLimit();
+            for (String attr : bagOfWords.subList(0, limit)) {
+                bw.write("@attribute <" + attr + "> integer");
+                bw.newLine();
+            }
+
+            String joined = "";
+            for (int i = 0; i < subjects.size(); i++) {
+                joined += subjects.get(i);
+                if (i != subjects.size() - 1) {
+                    joined += ",";
+                }
+            }
+
+            bw.write("@attribute classes {" + joined + "}");
+            bw.newLine();
+            bw.newLine();
+            bw.write("@data");
+            bw.newLine();
+
+        } catch (IOException ex) {
+            Logger.getLogger(BagOfWords.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private static int getBagOfWordsLimit() {
+        int size = Integer.parseInt(prop.getProperty("BAG_OF_WORDS_SIZE"));
+        if (size == 0) {
+            size = bagOfWords.size();
+        }
+
+        return size;
+    }
 }
--- src/PreProcessamento.java
+++ src/PreProcessamento.java
@@ -1,47 +1,84 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.Formatter;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.Properties;
 
 public class PreProcessamento {
+    private static Properties prop;
+    private static InputStream input = null;
+
+    private static File dirFiles;
+    private static List<File> textFolders = new ArrayList<>();
+    private static ArrayList<String> listStopWords = new ArrayList<>();
+    private static List<ArrayList<String>> listao = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
-        String pathStopList, pathDir;
-        File dirFiles, stopList;
+        prop = new Properties();
+        input = new FileInputStream(System.getProperty("user.dir") + "\\src\\config.properties");
+        prop.load(input);
 
-        System.out.println("Caminho da StopWords:");
-        pathStopList = buf.readLine();
-        stopList = new File(pathStopList);
+        System.out.println("Lendo StopWords:");
+        readStopListFile();
 
-        System.out.println("Caminho dos Arquivos:");
-        pathDir = buf.readLine();
-        dirFiles = new File(pathDir);
+        System.out.println("Lendo Arquivos:");
+        readTextFiles();
 
-        List<File> files = new ArrayList<>();
-
-        //Cria List com arquivos da pasta designada
-        for (File file : dirFiles.listFiles()) {
-            files.add(file);
+        for (final File fileEntry : textFolders) {
+            if (fileEntry.isDirectory()) {
+                makeProcess(Arrays.asList(fileEntry.listFiles()));
+            }
         }
+    }
 
-        //Cria list com as stopWords do arquivo
-        ArrayList<String> listStopWords = new ArrayList<>();
+    public static boolean deleteDirectory(File path) {
+        if (path.exists()) {
+            File[] files = path.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isDirectory()) {
+                    deleteDirectory(files[i]);
+                } else {
+                    files[i].delete();
+                }
+            }
+        }
+        return (path.delete());
+    }
+
+    public static File createDirectory(String path) {
         try {
-            FileReader fileReader = new FileReader(stopList);
+            File novaPasta = new File(path);
+            if (novaPasta.exists()) {
+                if (deleteDirectory(novaPasta)) {
+                    novaPasta.mkdir();
+                    novaPasta.setWritable(true);
+                    novaPasta.setReadable(true);
+                    novaPasta.setExecutable(true);
+                }
+            }
 
-            BufferedReader bufferedReader
-                    = new BufferedReader(fileReader);
+            return novaPasta;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private static void readStopListFile() {
+        //Cria list com as stopWords do arquivo
+        try {
+            FileReader fileReader = new FileReader(new File(prop.getProperty("pathStopList")));
+
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
             String linha = "";
             while ((linha = bufferedReader.readLine()) != null) {
                 listStopWords.add(linha);
@@ -51,100 +88,83 @@ public class PreProcessamento {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        //Crias lista de lista de Strings com todos os arquivos da pasta designada
-        List<ArrayList<String>> listao = new ArrayList<>();
+    private static void readTextFiles() {
+        dirFiles = new File(prop.getProperty("pathSubjects"));
+        for (final File fileEntry : dirFiles.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                textFolders.add(fileEntry);
+            }
+        }
+    }
 
-        for (File file : files) {
-            ArrayList<String> arqFiles = new ArrayList<>();
-            try {
-                FileReader fileReader = new FileReader(file);
-
-                BufferedReader bufferedReader
-                        = new BufferedReader(fileReader);
+    private static void listOriginalWord(File file) {
+        ArrayList<String> arqFiles = new ArrayList<>();
+        try {
+            BufferedReader bufferedReader;
+            try (FileReader fileReader = new FileReader(file)) {
+                bufferedReader = new BufferedReader(fileReader);
                 String linha = "";
                 while ((linha = bufferedReader.readLine()) != null) {
-                    linha = linha.replaceAll("[^A-Za-z]", "");
+                    linha = linha.replaceAll("[^A-Za-z ]", "");
 
                     arqFiles.add(linha);
                 }
-                fileReader.close();
-                bufferedReader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-            listao.add(arqFiles);
-        }
-
-        File novaPasta = new File(dirFiles + "\\retorno");
-        if(novaPasta.exists()){
-            novaPasta.delete();
-        }
-        novaPasta.mkdir();
-        int nomeArq = 1;
-        ArrayList<String> allFiles = new ArrayList<String>();
-        for (ArrayList<String> l : listao) {
-
-            //arquivos menos listStopWords
-            l.removeAll(listStopWords);
-
-        //cria arquivo de teste
-        try {
-            FileWriter fw = new FileWriter(novaPasta + "\\" + String.valueOf(nomeArq) + ".txt", true);
-            nomeArq++;
-            BufferedWriter bw = new BufferedWriter(fw);
-
-            for (String s : l) {
-
-                if (!s.isEmpty() && !s.equals("")) {
-                    allFiles.add(s);
-                    bw.write(s);
-                    bw.newLine();
-                }
-            }
-            bw.close();
-            fw.close();
-
+            bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        listao.add(arqFiles);
     }
 
-    Collections.sort (allFiles);
+    private static ArrayList<String> createClearedFiles(File novaPasta) {
+        int nomeArq = 1;
+        ArrayList<String> allFiles = new ArrayList<>();
 
-    ArrayList<String> rank = new ArrayList<String>();
+        for (ArrayList<String> l : listao) {
+            //arquivos menos listStopWords
+            l.removeAll(listStopWords);
+            //cria arquivo de teste
+            try {
+                FileWriter fw = new FileWriter(novaPasta + "\\" + String.valueOf(nomeArq) + ".txt", true);
+                nomeArq++;
+                BufferedWriter bw = new BufferedWriter(fw);
 
-    HashSet<String> allFilesUniqueHash = new HashSet<String>();
+                for (String s : l) {
 
-    allFilesUniqueHash.addAll (allFiles);
+                    if (!s.isEmpty() && !s.equals("")) {
+                        allFiles.add(s);
+                        bw.write(s);
+                        bw.newLine();
+                    }
+                }
+                bw.close();
+                fw.close();
 
-    ArrayList<String> allFilesUnique = new ArrayList<String>();
-    
-    Collections.sort(allFilesUnique);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-    allFilesUnique.addAll (allFilesUniqueHash);
-
-    for (int i = 0;i< allFilesUnique.size ();i++) {
-            int cont = 0;
-        /*for (int j = 0; j <= allFiles.size(); j++) {
-            if (allFilesUnique.get(i).equals(allFiles.get(j))) {
-                Collections.
-                cont++;
-            } 
-        }*/
-        rank.add(allFilesUnique.get(i) + ";" + Collections.frequency(allFiles, allFilesUnique.get(i)));
-
+        return allFiles;
     }
-    //rank.toString();
-    
-    try {
+
+    private static void makeRankFile(File novaPasta, ArrayList<String> allFiles, ArrayList<String> allFilesUnique) {
+        ArrayList<String> rank = new ArrayList<>();
+
+        for (int i = 0; i < allFilesUnique.size(); i++) {
+            rank.add(allFilesUnique.get(i) + ";" + Collections.frequency(allFiles, allFilesUnique.get(i)));
+        }
+
+        try {
             FileWriter fw = new FileWriter(novaPasta + "\\" + "ranking.txt", true);
             BufferedWriter bw = new BufferedWriter(fw);
 
             for (String s : rank) {
-
-                    bw.write(s);
-                    bw.newLine();
+                bw.write(s);
+                bw.newLine();
             }
             bw.close();
             fw.close();
@@ -152,7 +172,28 @@ public class PreProcessamento {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-}
+    private static void makeProcess(List<File> files) {
+        HashSet<String> allFilesUniqueHash = new HashSet<>();
+        ArrayList<String> allFilesUnique = new ArrayList<>();
 
+        //Crias lista de lista de Strings com todos os arquivos da pasta designada        
+        for (File file : files) {
+            if (file.isFile()) {
+                listOriginalWord(file);
+            }
+        }
+
+        //Cria pasta com textos sem stopwords
+        File novaPasta = createDirectory(files.get(0).getPath());
+
+        //Cria os arquivos de texto com palavras relevantes - sem stopwords
+        ArrayList<String> allFiles = createClearedFiles(novaPasta);
+        allFilesUniqueHash.addAll(allFiles);
+        Collections.sort(allFilesUnique);
+        allFilesUnique.addAll(allFilesUniqueHash);
+
+        makeRankFile(novaPasta, allFiles, allFilesUnique);
+    }
 }
